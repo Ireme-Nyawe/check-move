2c2a8717f94846a89458d093ce3ffa11f7ee28ce
Ireme-Nyawe <akimanaj41@gmail.com>
2023-11-23 07:58:00 +0200

    add needed folder

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 const port = process.env.PORT;
 app.listen(port, ()=>{
     console.log(`server is running on http://localhost:${port}`);
