2438aaa3fd22fed6cc86a59b44a1e0ef2f12c3bb
Ireme-Nyawe <akimanaj41@gmail.com>
2023-11-27 10:41:51 +0200

    connect to db

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import app from "./app.js";
+import mongoose from "mongoose";
+
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.DBC)
+.then(()=>{
+    console.log("Good Job, DB Connected.");
+}).catch((error)=> {console.log(error)});
 
